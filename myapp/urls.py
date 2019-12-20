4ce5b8cedafb87cfcacd2cdc15e3f825536b1914
EeshaArif <eeshaarif@gmail.com>
2019-12-21 03:30:10 +0500

    fixed-detect-button-response

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    url(r'^detect$', views.detect),
+    url(r'^detect$', views.detect, name='detect'),
     path('accounts/profile/',views.profile, name='profile'),
     path('accounts/profile/history',views.patienthistory, name='history'),
     path('accounts/profile/display', views.search, name = 'display'),
